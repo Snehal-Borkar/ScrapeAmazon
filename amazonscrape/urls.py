b051c2fb60e69f60cabd78b49465fc862337a13a
SNEHAL BORKAR <Snehal Borkar>
2021-08-08 23:59:29 +0530

    staticfile set

--- amazonscrape/urls.py
+++ amazonscrape/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from top_product import views
-
-# app_name ='top_product'
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +25,4 @@ urlpatterns = [
     path('scrapdata/',views.scrapdata),
     path('search/',views.search,name='search'),
     path('search/<int:id>/',views.searched,name="detail")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
